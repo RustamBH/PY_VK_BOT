4dd1e5f3fc74b3aaf3e2f2e55b0e69d70ae5e7e4
Sergey Gudik <sergey.goodic@gmail.com>
2022-05-23 23:21:24 +0300

    add sql script to create DB

--- sql/create.sql
+++ sql/create.sql
@@ -1,1 +1,16 @@
-Тут буду скрипты для инициализации БД
+DROP DATABASE IF EXISTS PY_VK_BOT;
+CREATE DATABASE PY_VK_BOT;
+CREATE USER pyvkbot with password 'pyvkbot';
+ALTER DATABASE PY_VK_BOT OWNER TO pyvkbot;
+CREATE TABLE users (id int primary key,
+                   name varchar(40) not Null,
+                   city varchar(40),
+                   sex boolean,
+                   age int not Null,
+                   lm_date date not Null);
+CREATE TABLE userFotos (userid int primary key references users(id),
+                       link varchar(255) not Null);
+CREATE TABLE pairs (userid int not Null references users(id),
+                    pairid int not Null,
+                   constraint pk primary key (userid, pairid));
+					   

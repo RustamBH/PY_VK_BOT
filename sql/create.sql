19d742f2ce64a49515e29f842b098208a604f27e
Sergey Gudik <sergey.goodic@gmail.com>
2022-05-30 20:21:57 +0300

    fixed foto problem

--- sql/create.sql
+++ sql/create.sql
@@ -2,14 +2,17 @@ DROP DATABASE IF EXISTS PY_VK_BOT;
 CREATE DATABASE PY_VK_BOT;
 CREATE USER pyvkbot with password 'pyvkbot';
 ALTER DATABASE PY_VK_BOT OWNER TO pyvkbot;
+GRANT ALL PRIVILEGES ON DATABASE py_vk_bot TO pyvkbot;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO pyvkbot;
 CREATE TABLE users (id int primary key,
                    name varchar(40) not Null,
                    city varchar(40),
                    sex boolean,
                    age int not Null,
+				   profile text,
 				   last_seen int,
                    update_time date not Null);
-CREATE TABLE userFotos (userid int primary key references users(id),
+CREATE TABLE userFotos (userid int not Null,
                        link varchar(255) not Null);
 CREATE TABLE pairs (userid int not Null references users(id),
                     pairid int not Null,
